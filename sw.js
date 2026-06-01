1843c91410006c7fec23fa03daf084381dcef48b
D30P-creator <demi.pacca97@gmail.com>
2026-06-01 18:56:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump questa versione a ogni aggiornamento dei file per forzare il refresh.
-const CACHE = 'nobrok-v17';
+const CACHE = 'nobrok-v19';
 const ASSETS = ['./index.html', './quick-add.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
